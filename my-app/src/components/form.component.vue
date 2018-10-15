796f063378dbae75b35c8767e01b270054d8957d
Sebastian Springer <info@sebastian-springer.com>
2018-10-15 14:16:33 +0200

    error handling

--- my-app/src/components/form.component.vue
+++ my-app/src/components/form.component.vue
@@ -1,8 +1,17 @@
 <template>
   <form @submit.prevent="handleSubmit">
-    Name: <input type="text" v-model="name" /><br />
-    is Admin:   <input type="checkbox" v-model="isAdmin"> <br />
-    <button type="submit">submit</button>
+    <div>
+      <label for="name">Name:</label>
+      <input @change="checkForm" :class="{error: !!errors.name}" id="name" v-model="name">
+      <div class="errorMessage" v-if="errors.name">{{errors.name}}</div>
+    </div>
+    <div>
+      <label for="isAdmin">Is Admin:</label>
+      <input type="checkbox" v-model="isAdmin"> 
+    </div>
+    <div>
+      <button type="submit">submit</button>
+    </div>
   </form>
 </template>
 
@@ -11,14 +20,37 @@ export default {
   name: 'Form',
   data() {
     return {
+      errors: { name: null },
       name: '',
       isAdmin: false,
     };
   },
   methods: {
+    checkForm() {
+      if (this.name.length === 0) {
+        this.errors.name = 'Name is a required field';
+      } else {
+        this.errors.name = null;
+      }
+    },
     handleSubmit() {
-      this.$emit('onSave', { name: this.name, isAdmin: this.isAdmin });
+      this.checkForm();
+      if (!this.errors.name) {
+        this.$emit('onSave', { name: this.name, isAdmin: this.isAdmin });
+        this.name = '';
+        this.isAdmin = false;
+      }
     },
   },
 };
 </script>
+
+<style scoped>
+.error {
+  border: 1px solid red;
+}
+.errorMessage {
+  color: red;
+  font-weight: bold;
+}
+</style>
