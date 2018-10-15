204560c85e01d74f4d51ae8bf2ce961090140d26
Sebastian Springer <info@sebastian-springer.com>
2018-10-15 09:31:06 +0200

    loop over a list of user objects

--- my-first-vue-app/index.js
+++ my-first-vue-app/index.js
@@ -2,6 +2,10 @@ const app = new Vue({
   el: '#myApp',
   data: {
     name: 'Basti',
-    users: [{ id: 1, name: 'Peter' }, { id: 2, name: 'Paul' }],
+    users: [
+      { id: 1, name: 'Peter' },
+      { id: 2, name: 'Paul' },
+      { id: 3, name: 'Mary' },
+    ],
   },
 });
