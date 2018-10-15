40081b56f18c14a27c587a650e7f0764d81d0003
Sebastian Springer <info@sebastian-springer.com>
2018-10-15 10:02:46 +0200

    component

---
+++ my-components/users.component.js
@@ -0,0 +1,25 @@
+Vue.component('users', {
+  data() {
+    return {
+      name: 'Basti',
+      users: [
+        { id: 1, name: 'Peter' },
+        { id: 2, name: 'Paul' },
+        { id: 3, name: 'Mary' },
+      ],
+    };
+  },
+  methods: {
+    handleClick(user, event) {
+      alert(user.name);
+    },
+  },
+  template: `
+    <ul>
+      <li v-for="user in users" v-on:click="handleClick(user, $event)">
+        ({{user.id}})
+        {{user.name}}
+      </li>
+    </ul>
+    `,
+});
